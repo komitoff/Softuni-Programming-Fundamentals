9b8596940755b02928e767a9d192611ff3b70072
tobieski <todorkomitov@gmail.com>
2017-01-17 19:09:56 +0200

    Task elevator finished.

---
+++ DataTypesAndVariablesHomework/4. Elevator/Elevator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Elevator
+{
+    static void Main()
+    {
+        int numberOfPeople = int.Parse(Console.ReadLine());
+        int capacity = int.Parse(Console.ReadLine());
+        int courses = 0;
+
+        if (capacity >= numberOfPeople)
+        {
+            Console.WriteLine(1);
+        }
+        else if (numberOfPeople % capacity == 0)
+        {
+            courses = numberOfPeople / capacity;
+
+            Console.WriteLine(courses);
+        }
+        else
+        {
+            courses = numberOfPeople / capacity + 1;
+            Console.WriteLine(courses);
+        }
+    }
+}
