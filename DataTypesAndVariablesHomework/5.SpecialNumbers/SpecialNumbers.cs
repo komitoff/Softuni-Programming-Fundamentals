4f8582c7a8737065f9ab54f1bdb386cf9886c8c2
tobieski <todorkomitov@gmail.com>
2017-01-17 19:22:36 +0200

    Task special numbers finished.

---
+++ DataTypesAndVariablesHomework/5.SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class SpecialNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = 0;
+        int sumOfDigits = 0;
+        bool flag = false;
+
+        for (int i = 0; i <= n; i++)
+        {
+            m = i;
+            while (m != 0)
+            {
+                sumOfDigits += m % 10;
+                m /= 10;
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    flag = true; 
+                }
+            }
+
+            if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+            {
+                Console.WriteLine("{0} -> True", i);
+                flag = false;
+            }
+            else
+            {
+                Console.WriteLine("{0} -> False", i);
+            }
+
+            sumOfDigits = 0;
+        }
+    }
+}
