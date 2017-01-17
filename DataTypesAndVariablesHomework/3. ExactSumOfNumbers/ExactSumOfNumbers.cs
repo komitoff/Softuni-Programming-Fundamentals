b41647c6497429fa821c66369313ea2a3e5f2317
tobieski <todorkomitov@gmail.com>
2017-01-17 18:56:53 +0200

    (Commit from visual studio) Did the task exact sum of numbers.

---
+++ DataTypesAndVariablesHomework/3. ExactSumOfNumbers/ExactSumOfNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ExactSumOfNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        decimal result = 0m;
+        for (int i = 0; i < n; i++)
+        {
+            decimal current = decimal.Parse(Console.ReadLine());
+            result += current;
+
+        }
+        Console.WriteLine(result);
+    }
+}
