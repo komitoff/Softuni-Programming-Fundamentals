c48ff06b070db460de06cb9cc5e0da127b7d88cd
tobieski <todorkomitov@gmail.com>
2017-01-17 18:52:38 +0200

    Did the second task from the homework ( circle area - 12 digits precision)

---
+++ DataTypesAndVariablesHomework/2. CircleArea/CircleArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class CircleArea
+{
+    static void Main()
+    {
+        double radius = double.Parse(Console.ReadLine());
+
+        double area = Math.PI * radius * radius;
+
+        Console.WriteLine("{0:f12}", area);
+    }
+}
