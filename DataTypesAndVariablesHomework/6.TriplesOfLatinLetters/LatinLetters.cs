9559a0d0b6ba2179bc05103d7e036a82b411d10d
tobieski <todorkomitov@gmail.com>
2017-01-17 19:31:23 +0200

    Task triples of latin letters - done.

---
+++ DataTypesAndVariablesHomework/6.TriplesOfLatinLetters/LatinLetters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class LatinLetters
+{
+    static void Main()
+    {
+        int nn = int.Parse(Console.ReadLine());
+
+        char n = Convert.ToChar(nn + 97);
+
+        for (char i = 'a'; i < n; i++)
+        {
+            for (char j = 'a'; j < n; j++)
+            {
+                for (char k = 'a'; k < n; k++)
+                {
+                    Console.WriteLine("{0}{1}{2}", i, j, k);
+                }
+            }
+        }
+    }
+}
