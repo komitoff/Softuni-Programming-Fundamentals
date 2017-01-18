40090abb69e56fa06422abc74051d80a0d45e695
tobieski <todorkomitov@gmail.com>
2017-01-18 21:02:41 +0200

    task Greetings - done! (for some reason, the dollar syntax does not work)

---
+++ DataTypesAndVariablesHomework/7.Greetings/Greetings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Greetings
+{
+    static void Main()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        string age = Console.ReadLine();
+
+        //For some reasong using $ infront of the string does not work
+        //possible reason - using visual studio 2013
+        Console.WriteLine("Hello, {0} {1}.\r\n You are {2} years old.",firstName, lastName, age);
+    }
+}
