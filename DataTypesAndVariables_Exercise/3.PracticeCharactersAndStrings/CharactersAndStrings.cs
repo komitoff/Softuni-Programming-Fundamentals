a5a5a8bed40479ba73e150017c5a8b0812334dfd
komitoff <tobo_91@abv.bg>
2017-01-18 21:48:48 +0200

    task for practicing the primitive data types is done.

---
+++ DataTypesAndVariables_Exercise/3.PracticeCharactersAndStrings/CharactersAndStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class CharactersAndStrings
+{
+    static void Main()
+    {
+            //Software University
+            //B
+            //y
+            //e
+            //I love programming
+
+        string softuni = "Software University";
+        Console.WriteLine(softuni);
+        char letterB = 'B';
+        Console.WriteLine(letterB);
+        char letterY = 'y';
+        Console.WriteLine(letterY);
+        char letterE = 'e';
+        Console.WriteLine(letterE);
+        string programmingLove = "I love programming";
+        Console.WriteLine(programmingLove);
+
+    }
+}
