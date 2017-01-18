50c5b440622561f74de701f60dd70acf2c850ade
komitoff <tobo_91@abv.bg>
2017-01-18 21:37:37 +0200

    tasks 1 and 2 from homework - Done!

---
+++ DataTypesAndVariables_Exercise/1.PractiseIntegers/PracticeIntegers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class PracticeIntegers
+{
+    static void Main()
+    {
+        //-100
+        //128
+        //-3540
+        //64876
+        //2147483648
+        //-1141583228
+        //-1223372036854775808
+
+        short firstNumber = -100;
+        Console.WriteLine(firstNumber);
+        byte secondNumber = 128;
+        Console.WriteLine(secondNumber);
+        short thirdNumber = -3540;
+        Console.WriteLine(thirdNumber);
+        int fourthNumber = 64876;
+        Console.WriteLine(fourthNumber);
+        long fifthNumber = 2147483648;
+        Console.WriteLine(fifthNumber);
+        int sixthNumber = -1141583228;
+        Console.WriteLine(sixthNumber);
+        long finalNumber = -1223372036854775808;
+        Console.WriteLine(finalNumber);
+    }
+}
---
+++ DataTypesAndVariables_Exercise/2.PracticeFloatingPointNumbers/FloatingPointNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class FloatingPointNumbers
+{
+    static void Main()
+    {
+        //3.141592653589793238
+        //1.60217657
+        //7.8184261974584555216535342341
+
+        decimal firstNumber = 3.141592653589793238m;
+        Console.WriteLine(firstNumber);
+        double secondNumber = 1.60217657;
+        Console.WriteLine(secondNumber);
+        decimal thirdNumber = 7.8184261974584555216535342341m;
+        Console.WriteLine(thirdNumber);
+
+
+    }
+}
