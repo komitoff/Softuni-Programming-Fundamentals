a51d448f459683802db6aade769869491b91b770
tobieski <todorkomitov@gmail.com>
2017-01-18 21:10:05 +0200

    task - Refactor Pyrmid Volume program - Done !

---
+++ DataTypesAndVariablesHomework/8.VolumeOfPyramid/PyramidVolume.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class PyramidVolume
+{
+    static void Main()
+    {
+        double length = 0;
+        double height = 0;
+        double width = 0;
+        double volume = 0;
+        Console.Write("Length: ");
+        length = double.Parse(Console.ReadLine());
+        Console.Write("Width: ");
+        width = double.Parse(Console.ReadLine());
+        Console.Write("Height: ");
+        height = double.Parse(Console.ReadLine());
+        volume = (length + height + width) / 3;
+        Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+
+    }
+}
