09f64afd48e42304fc425802d4731272f9609dbd
komitoff <tobo_91@abv.bg>
2017-01-21 15:40:50 +0200

    Task 13 and 14 - done. (not checked in judge, cause of judge not respondin).

---
+++ DataTypesAndVariables_Exercise/13.DigitOrSymbol/DigitOrSymbol.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+class DigitOrSymbol
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine().ToLower();
+
+        byte[] asciiValue = Encoding.ASCII.GetBytes(input.ToCharArray());
+
+        //check if it is vowel
+        if (asciiValue[0] == 97   ||
+            asciiValue[0] == 101  ||
+            asciiValue[0] == 111  ||
+            asciiValue[0] == 105  ||
+            asciiValue[0] == 117  ||
+            asciiValue[0] == 121)
+        {
+            Console.WriteLine("vowel");
+        }
+        else if (asciiValue[0] >= 48 || asciiValue[0] <= 57)
+        {
+            Console.WriteLine("digit");
+        }
+        else
+        {
+            Console.WriteLine("other");
+        }
+
+
+    }
+}
---
+++ DataTypesAndVariables_Exercise/14.IntegerToHexAndBinary/IntegerToHexAndBinary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class IntegerToHexAndBinary
+{
+    static void Main()
+    {
+        int input = int.Parse(Console.ReadLine());
+        string hex = Convert.ToString(input, 16);
+        string binary = Convert.ToString(input, 2);
+        Console.WriteLine(hex.ToUpper());
+        Console.WriteLine(binary.ToUpper());
+    }
+}
