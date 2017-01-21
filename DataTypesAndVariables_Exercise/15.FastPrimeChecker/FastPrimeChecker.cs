3b2c8820cb9bd6f5e53ff2639dbbbf58d47fa8a9
komitoff <tobo_91@abv.bg>
2017-01-21 15:51:14 +0200

    Task 14 - done. Refactoring the fast prime checker. Solved with different algorith.

---
+++ DataTypesAndVariables_Exercise/15.FastPrimeChecker/FastPrimeChecker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class FastPrimeChecker
+{
+    static void Main()
+    {
+        //int ___Do___ = int.Parse(Console.ReadLine());
+        //for (int DAVIDIM = 0; DAVIDIM <= ___Do___; DAVIDIM++)
+        //{
+        //    bool TowaLIE = true;
+        //    for (int delio = 2; delio <= Math.Sqrt(DAVIDIM); delio++)
+        //    {
+        //        if (DAVIDIM % delio == 0)
+        //        {
+        //            TowaLIE = false;
+        //            break;
+        //        }
+        //    }
+        //    Console.WriteLine($"{DAVIDIM} is prime -> {TowaLIE}");
+        //}
+
+        int length = int.Parse(Console.ReadLine());
+        int counter = 0;
+        for (int i = 1; i <= length; i++)
+        {
+            bool isPrime = false; 
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    counter++;
+                }
+            }
+            
+            if (counter <= 2)
+            {
+                isPrime = true;
+            }
+            counter = 0;
+            Console.WriteLine($"{i} is prime -> {isPrime}");
+        }
+
+    }
+}
+
