5602a8bc2a5537b06c181f16df8ba11e7ae70bca
komitoff <tobo_91@abv.bg>
2017-01-23 20:42:55 +0200

    Finished task Multiply of even and odd digits in an integer number.

---
+++ MethodsAndDebugging_Lect/8.MultiplyEvensByOdds/MultiplyEvensByOdds.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MultiplyEvensByOdds
+{
+    static void Main(string[] args)
+    {
+        int input = int.Parse(Console.ReadLine());
+        Console.WriteLine(GetMultipleOfEvenAndOdds(Math.Abs(input)));
+    }
+
+    /// <summary>
+    /// Returns the multiple of the summ of all even and odd digits
+    /// </summary>
+    /// <param name="input">Integer number</param>
+    /// <returns></returns>
+    private static int GetMultipleOfEvenAndOdds(int input)
+    {
+        return GetSumOfEvenDigits(input) * GetSumOfOddDigits(input);
+    }
+
+    /// <summary>
+    /// Returns the summ of all even digits in the given number
+    /// </summary>
+    /// <param name="n">Integer number</param>
+    /// <returns></returns>
+    private static int GetSumOfEvenDigits(int n)
+    {
+        int sum = 0;
+
+        while (n > 0)
+        {
+            if (n % 2 == 0)
+            {
+                sum += (n % 10);
+            }
+            n /= 10;
+        }
+        return sum;
+    }
+
+    /// <summary>
+    /// Returns the sum of all odd digits in the given number
+    /// </summary>
+    /// <param name="n">Integer number</param>
+    /// <returns></returns>
+    private static int GetSumOfOddDigits(int n)
+    {
+        int sum = 0;
+        while (n > 0)
+        {
+            if (n % 2 != 0)
+            {
+                sum += (n % 10);
+            }
+            n /= 10;
+        }
+        return sum;
+    }
+}
