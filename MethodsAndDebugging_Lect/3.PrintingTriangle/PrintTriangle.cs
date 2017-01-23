5c05815f2e6cfe88a40371cad5eaa044f75d753f
komitoff <tobo_91@abv.bg>
2017-01-23 20:01:46 +0200

    Finish with part 1 - Declaring and Invoking methods.

---
+++ MethodsAndDebugging_Lect/3.PrintingTriangle/PrintTriangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class PrintTriangle
+{
+    static void Main(string[] args)
+    {
+        int length = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i < length; i++)
+        {
+            PrintLine(1, i);
+        }
+
+        PrintLine(1, length);
+
+        for (int i = length - 1; i >= 0; i--)
+        {
+            PrintLine(1, i);
+        }
+    }
+
+    private static void PrintLine(int v, int length)
+    {
+        for (int i = 1; i <= length; i++)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ MethodsAndDebugging_Lect/4.DrawFilledSquare/DrawFilledSquare.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class DrawFilledSquare
+{
+    static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+
+        PrintHeader(length);
+        for (int i = 0; i < length - 2; i++)
+        {
+            PrintBody(length);
+        }
+        PrintFooter(length);
+        
+    }
+
+    /// <summary>
+    /// Prints the Header of the square
+    /// </summary>
+    /// <param name="length"></param>
+    private static void PrintHeader(int length)
+    {
+        Console.Write(new string('-', length * 2));
+        Console.WriteLine();
+    }
+
+    /// <summary>
+    /// Prints the body of the square
+    /// </summary>
+    /// <param name="length"></param>
+    private static void PrintBody(int length)
+    {
+        Console.Write("-");
+        for (int i = 0; i < length - 1; i++)
+        {
+            Console.Write("\\/");
+        }
+        Console.Write("-");
+        Console.WriteLine();
+    }
+
+    /// <summary>
+    /// Prints the footer of the square
+    /// </summary>
+    /// <param name="length">length of the row</param>
+    private static void PrintFooter(int length)
+    {
+        Console.Write(new string('-', length * 2));
+    }
+
+
+}
