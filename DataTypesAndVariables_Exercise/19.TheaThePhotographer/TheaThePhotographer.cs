5e9c3b46f38191def72495ee271377efd37ee8ab
komitoff <tobo_91@abv.bg>
2017-01-23 19:37:04 +0200

    Start with new homework (from methods and debugging).

---
+++ DataTypesAndVariables_Exercise/19.TheaThePhotographer/TheaThePhotographer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class TheaThePhotographer
+{
+    static void Main()
+    {
+        int picturesTaken = int.Parse(Console.ReadLine());
+        int filteringTime = int.Parse(Console.ReadLine());
+        int usefulPictures = int.Parse(Console.ReadLine());
+        int uploadTime = int.Parse(Console.ReadLine());
+
+
+    }
+}
