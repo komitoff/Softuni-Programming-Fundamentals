5910ca38e4738b1c3cdf2fc795a2534a742ab8ac
komitoff <tobo_91@abv.bg>
2017-01-25 19:52:00 +0200

    task prime numbers - done .

---
+++ MethodsDebugging_Exercise/E6.PrimeChecker/PrimeChecker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class PrimeChecker
+{
+    static void Main(string[] args)
+    {
+        long input = long.Parse(Console.ReadLine());
+
+        Console.WriteLine(IsPrime(input));
+
+    }
+
+
+    /// <summary>
+    /// Returns true if the given number is prime
+    /// and false, if it is not.
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    private static bool IsPrime(long number)
+    {
+        var boundary = (long)Math.Floor(Math.Sqrt(number));
+        if (number == 0 || number == 1)
+            return false;
+        for (int i = 2; i <= boundary; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
