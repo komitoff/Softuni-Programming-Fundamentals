10e1c4bd5ce237eb8ba3e5364f1c44171f57ef1c
komitoff <tobo_91@abv.bg>
2017-01-25 19:36:31 +0200

    did tasks 4 and 5 - numbers in reversed order and the Nth number from Fibonacci sequence.

---
+++ MethodsDebugging_Exercise/4.NumbersInReversedOrder/NumbersInReversedOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class NumbersInReversedOrder
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(Reverse(input));
+    }
+
+    /// <summary>
+    /// Returns reversed string
+    /// </summary>
+    /// <param name="input">String parameter</param>
+    /// <returns></returns>
+    private static string Reverse(string input)
+    {
+        char[] output = input.ToCharArray();
+        Array.Reverse(output);
+        return new string(output);
+    }
+}
---
+++ MethodsDebugging_Exercise/E5.FibonacciElements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E5.FibonacciElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fib(n));
+        }
+
+
+        /// <summary>
+        /// Returns the n number from the Fibonacci sequence
+        /// This method uses recursion in order to solve the problem 
+        /// it is not recomended to use recursion for this problem !
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private static long Fib(long n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+            else
+                return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
