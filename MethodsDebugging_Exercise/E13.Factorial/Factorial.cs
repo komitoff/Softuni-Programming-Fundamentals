ef9b66c1adfb277f8d72b7512583b0ab87dd38b9
komitoff <tobo_91@abv.bg>
2017-01-28 13:13:30 +0200

    Tasks factorial numbers and factorial trailing zeros - done (but not checked - judge not working atm).

---
+++ MethodsDebugging_Exercise/E13.Factorial/Factorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+class Factorial
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(FactorialNumbers(n));
+    }
+
+    private static BigInteger FactorialNumbers(int n)
+    {
+        BigInteger factorial = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+}
---
+++ MethodsDebugging_Exercise/E14.FactorialTrailingZeros/FactorialTrailingZeros.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+class FactorialTrailingZeros
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        BigInteger factorial = FactorialNumber(n);
+        Console.WriteLine(TrailingZeros(factorial));
+    }
+
+    private static int TrailingZeros(BigInteger number)
+    {
+
+        string factorial = number.ToString();
+        MatchCollection zeros = Regex.Matches(factorial, "0+$");
+        StringBuilder matches = new StringBuilder();
+        foreach (var zero in zeros)
+        {
+            matches.Append(zero);
+        }
+        string trailingZeros = matches.ToString();
+        return trailingZeros.Length;
+    }
+
+    private static BigInteger FactorialNumber(int n)
+    {
+        BigInteger factorial = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+}
