7c4222823a464bb2d1abedaa40a67c58a353429e
komitoff <tobo_91@abv.bg>
2017-01-28 17:01:54 +0200

    Homework arrrays from exercises

---
+++ Arrays_Exercise/E1.LargestCommonEnd/LargestCommonEnd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class LargestCommonEnd
+{
+    static void Main(string[] args)
+    {
+        string[] firstArr = Console.ReadLine().Split(' ');
+        string[] secondArr = Console.ReadLine().Split(' ');
+        int length = Math.Min(firstArr.Length, secondArr.Length);
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        for (int i = 0; i < length; i++)
+        {
+            if (firstArr[i].Equals(secondArr[i]))
+            {
+                cnt1++;
+            }
+        }
+        Array.Reverse(firstArr);
+        Array.Reverse(secondArr);
+
+        for (int i = 0; i < length; i++)
+        {
+            if (firstArr[i].Equals(secondArr[i]))
+            {
+                cnt2++;
+            }
+        }
+        Console.WriteLine(Math.Max(cnt1, cnt2));
+    }
+}
