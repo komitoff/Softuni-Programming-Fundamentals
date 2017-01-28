7925133159d1d22cb8adab93883cbbdc439c6e07
komitoff <tobo_91@abv.bg>
2017-01-28 16:18:40 +0200

    Task 8 - done.

---
+++ Arrays_Lab/E8.CondensedNumbers/CondensedNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CondensedNumbers
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int length = input.Length;
+        int[] nums = new int[length];
+        int[] condensed = new int[length - 1];
+
+        for (int i = 0; i < length; i++)
+        {
+            nums[i] = int.Parse(input[i]);
+        }
+
+        int condensedLen = condensed.Length;
+        if (length > 1)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < condensedLen; j++)
+                {
+                    condensed[j] = nums[j] + nums[j + 1];
+                }
+                nums = condensed;
+                condensedLen--;
+            }
+            Console.WriteLine(condensed[0]);
+        }
+        else
+        {
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
