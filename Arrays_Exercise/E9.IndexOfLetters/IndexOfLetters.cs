83c5012b3e4bcc6d0f204fadc15a4f7fd6533463
komitoff <tobo_91@abv.bg>
2017-01-29 16:07:57 +0200

    Positon of letter.

---
+++ Arrays_Exercise/E9.IndexOfLetters/IndexOfLetters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class IndexOfLetters
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        char[] letters = input.ToCharArray();
+        for (int i = 0; i < letters.Length; i++)
+        {
+            int position = (int)letters[i] - 97;
+            Console.WriteLine($"{letters[i]} -> {position}");
+        }
+        
+    }
+}
