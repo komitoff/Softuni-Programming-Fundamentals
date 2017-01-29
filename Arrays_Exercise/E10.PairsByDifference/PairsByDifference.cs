15f4a65bf1a299c82334674701bfcabe613744cb
komitoff <tobo_91@abv.bg>
2017-01-29 16:26:55 +0200

    Pairs of difference.

---
+++ Arrays_Exercise/E10.PairsByDifference/PairsByDifference.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class PairsByDifference
+{
+    static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int difference = int.Parse(Console.ReadLine());
+        int length = arr.Length;
+        int cnt = 0;
+
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = i + 1; j < length ; j++)
+            {
+                if (Math.Abs(arr[i] - arr[j]) == difference)
+                {
+                    cnt++;
+                }
+            }
+        }
+
+        if (length < 2 || cnt == 0)
+        {
+            Console.WriteLine(0);
+        }
+        else
+        {
+            Console.WriteLine(cnt);
+        }
+    }
+}
