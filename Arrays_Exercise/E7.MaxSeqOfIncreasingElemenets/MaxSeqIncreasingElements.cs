be0ddf14c3483c0ce940cd8d66fa318ead599b61
komitoff <tobo_91@abv.bg>
2017-01-29 15:10:58 +0200

    Maximal sequence of increasing elements in array.

---
+++ Arrays_Exercise/E7.MaxSeqOfIncreasingElemenets/MaxSeqIncreasingElements.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class MaxSeqIncreasingElements
+{
+    static void Main(string[] args)
+    {
+        int[] seq = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int previousElement = seq[0];
+        int currentElement = 0;
+        int startPosition = 0;
+        int bestStart = 0;
+        int currentCount = 1;
+        int bestCount = 0;
+
+        for (int i = 1; i < seq.Length; i++)
+        {
+            currentElement = seq[i];
+            if (currentElement > previousElement)
+            {
+                currentCount++;
+            }
+            else
+            {
+                currentCount = 1;
+                startPosition = i;
+            }
+
+            if (currentCount > bestCount)
+            {
+                bestCount = currentCount;
+                bestStart = startPosition;
+            }
+            previousElement = currentElement;
+        }
+
+        if (bestCount < seq.Length - 1)
+        {
+            for (int i = bestStart; i < bestCount + bestStart; i++)
+            {
+                Console.Write(seq[i] + " ");
+            }
+        }
+        else
+        {
+            for (int i = 0; i < seq.Length; i++)
+            {
+                Console.Write(seq[i] + " ");
+            }
+        }
+    }
+}
