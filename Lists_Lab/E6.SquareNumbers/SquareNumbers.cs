75ae8a0a0faf5517b72081ce9974b15836f3fbde
komitoff <tobo_91@abv.bg>
2017-01-30 20:41:17 +0200

    Square numbers in descening order.

---
+++ Lists_Lab/E6.SquareNumbers/SquareNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SquareNumbers
+{
+    static void Main()
+    {
+        List<double> numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+        numbers.Sort();
+        numbers.Reverse();
+        foreach (var number in numbers)
+        {
+            if (Math.Sqrt(number) == (int) (Math.Sqrt(number)))
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
