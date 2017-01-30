fe5a90955b80478b6475842a69cc5d9633439b7c
komitoff <tobo_91@abv.bg>
2017-01-30 20:14:15 +0200

    Simple lists exercise - remove and rotate from lists.

---
+++ Lists_Lab/E1.RemoveNegativeAndReverse/RemoveNegativeAndReverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class RemoveNegativeAndReverse
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        numbers.RemoveAll(i => i < 0);
+        numbers.Reverse();
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("empty");
+        }
+        foreach (var number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+
+        
+    }
+}
