d8a9a3a594a299fdf67e76cfba2402305ab984e7
komitoff <tobo_91@abv.bg>
2017-01-30 20:26:01 +0200

    Append some numbers to list.

---
+++ Lists_Lab/E2.AppendLists/AppendLists.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class AppendLists
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split('|').ToArray();
+        List<int> numbers = new List<int>();
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            numbers.AddRange(input[i].Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+        }
+
+        foreach (var number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
