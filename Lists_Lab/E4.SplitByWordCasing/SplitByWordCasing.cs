817382b6ac24fed7f198970018fa58762d0c3f3d
komitoff <tobo_91@abv.bg>
2017-02-01 20:10:14 +0200

    Split by word casing - 100/100 in judge.

--- Lists_Lab/E4.SplitByWordCasing/SplitByWordCasing.cs
+++ Lists_Lab/E4.SplitByWordCasing/SplitByWordCasing.cs
@@ -8,7 +8,7 @@ class SplitByWordCasing
     {
         String inputText = Console.ReadLine();
         string[] words = inputText
-                            .Split(new char[] { ',', '.', ';', ':', '!', '(', ')', '"', '\'', '\\', '/', '[', ']', ' '});
+                            .Split(new char[] { ',', '.', ';', ':', '!', '(', ')', '"', '\'', '\\', '/', '[', ']', ' '}, StringSplitOptions.RemoveEmptyEntries);
 
         List<string> lowerCase = new List<string>();
         List<string> upperCase = new List<string>();
@@ -19,11 +19,13 @@ class SplitByWordCasing
             string upCase = words[i].ToUpper();
             string lowCase = words[i].ToLower();
 
-            if (words[i].Equals(upCase))
+            //check if the word is the same as the same word, but upper Case
+            if (words[i].Equals(upCase) && !CheckForSymbols(words[i])) 
             {
                 upperCase.Add(words[i]);
             }
-            else if (words[i].Equals(lowCase))
+            //check if the word is the same as the same word, but lower Case
+            else if (words[i].Equals(lowCase) && !CheckForSymbols(words[i]))
             {
                 lowerCase.Add(words[i]);
             }
@@ -32,25 +34,47 @@ class SplitByWordCasing
                 mixedCase.Add(words[i]);
             }
         }
+        Console.WriteLine();
 
-        Console.Write("Lower case : ");
+        Console.Write("Lower-case: ");
         Console.WriteLine(string.Join(", ", lowerCase));
         //foreach (var word in lowerCase)
         //{
         //    Console.WriteLine(string.Join(" , ", word));
         //}
-        Console.Write("Mixed case : ");
+        Console.Write("Mixed-case: ");
         Console.WriteLine(string.Join(", ", mixedCase));
         //foreach (var word in mixedCase)
         //{
         //    Console.WriteLine(string.Join(" , ", word));
         //}
-        Console.Write("Upper case : ");
+        Console.Write("Upper-case: ");
         Console.WriteLine(string.Join(", ", upperCase));
         //foreach (var word in upperCase)
         //{
         //    Console.WriteLine(string.Join(" , ", word));
         //}
         
+    }
+
+    /// <summary>
+    /// Returns true if there is a special symbol.
+    /// Checks only for symbols less than 65 in the ascii table
+    /// cuase all other symbols are used for split.
+    /// </summary>
+    /// <param name="word"></param>
+    /// <returns></returns>
+    private static bool CheckForSymbols(string word)
+    {
+        char[] symbols = word.ToCharArray();
+        for (int i = 0; i < word.Length; i++)
+        {
+            if ((int) symbols[i] < 65)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
