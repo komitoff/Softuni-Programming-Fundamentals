a3bdcdf710063c622c0912a750f1953393c0cffa
komitoff <tobo_91@abv.bg>
2017-02-01 20:27:52 +0200

    Maximal sequence of equal elements.

---
+++ Lists_Exercise/E1.MaxSequenceOfIncreasingElements/MaxSequenceOfIncreasingElements.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MaxSequenceOfIncreasingElements
+{
+    static void Main()
+    {
+        //Refacotred the code from the previous exercise - best sequence of increasing elements
+        List<int> seq = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        int previousElement = seq[0];
+        int currentElement = 0;
+        int startPosition = 0;
+        int bestStart = 0;
+        int currentCount = 1;
+        int bestCount = 0;
+        int bestElement = 0;
+
+        for (int i = 1; i < seq.Count; i++)
+        {
+            currentElement = seq[i];
+            if (currentElement == previousElement)
+            {
+                currentCount++;
+            }
+            else
+            {
+                currentCount = 1;
+                startPosition = i;
+            }
+
+            if (currentCount > bestCount)
+            {
+                bestCount = currentCount;
+                bestStart = startPosition;
+                bestElement = seq[bestStart];
+            }
+            previousElement = currentElement;
+        }
+
+        if (bestCount > 1)
+        {
+            for (int i = bestStart; i < bestCount + bestStart; i++)
+            {
+                Console.Write(bestElement + " ");
+            }
+        }
+        else
+        {
+            Console.WriteLine(seq[0]);
+        }
+
+    }
+}
