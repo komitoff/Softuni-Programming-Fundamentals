4bb37a41cd3728b5d1e1791e77d82fbf8ea6bfcc
komitoff <tobo_91@abv.bg>
2017-02-03 19:32:20 +0200

    Bomb numbers - not finished.

---
+++ Lists_Exercise/E5.BombNumbers/BombNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class BombNumbers
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        int index = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        if (index < 0 || index >= numbers.Count)
+        {
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            Console.WriteLine(sum);
+        }
+        else
+        {
+
+        }
+
+    }
+}
