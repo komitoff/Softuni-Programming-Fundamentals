3c616ab51f998ab3ff3ea2d814d45162981dbb0c
komitoff <tobo_91@abv.bg>
2017-02-04 16:33:02 +0200

    Count Real Numbers -> hashmaps

---
+++ DictLambdaLINQ_Lab/E1.CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class CountRealNumbers
+{
+    static void Main()
+    {
+        List<double> numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+
+        Dictionary<double, int> count = new Dictionary<double, int>();
+        numbers.Sort();
+        foreach (var number in numbers)
+        {
+            if (!count.ContainsKey(number))
+            {
+                count.Add(number, 0);
+            }
+            count[number]++;
+        }
+
+        foreach (var item in count)
+        {
+            Console.Write(item.Key + " -> " + item.Value);
+            Console.WriteLine();
+        }
+    }
+}
