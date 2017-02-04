dbfd2196928d63f104973107a05223407c16a2f3
komitoff <tobo_91@abv.bg>
2017-02-04 16:23:29 +0200

    Bomb numbers 80/100.

--- Lists_Exercise/E5.BombNumbers/BombNumbers.cs
+++ Lists_Exercise/E5.BombNumbers/BombNumbers.cs
@@ -7,23 +7,56 @@ class BombNumbers
     static void Main()
     {
         List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-        int[] indexAndCount = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-        int index = indexAndCount[0];
-        int count = indexAndCount[1];
-        int sum = Bomb(numbers, index, count);
+        
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int specialNumb = input[0];
+        int count = input[1];
+        int sum = Bomb(numbers, specialNumb, count);
         Console.WriteLine(sum);
 
     }
 
-    private static int Bomb(List<int> numbers,int index, int count)
+
+    //1 2 2 4 2 2 4
+    //4 2 
+    private static int Bomb(List<int> numbers,int specialNumb, int count)
     {
         int sum = 0;
+        int startIndex = 0;
+        int endIndex = 0;
+
         if (count > 0 && count < numbers.Count)
         {
-            numbers.RemoveRange(index, count);
-            numbers.Reverse();
-            numbers.RemoveRange(index , count);
-            
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == specialNumb)
+                {
+                    if (i - count < 0)
+                    {
+                        startIndex = 0;
+                    }
+                    else
+                    {
+                        startIndex = i - count;
+                    }
+
+                    if (i + count >= numbers.Count)
+                    {
+                        endIndex = numbers.Count - 1;
+                    }
+                    else
+                    {
+                        endIndex = i + count;
+                    }
+
+                    for (int j = endIndex; j >= startIndex; j--)
+                    {
+                        numbers.RemoveAt(j);
+                    }
+                    i = 0;
+                }
+             
+            }
         }
 
         foreach (var number in numbers)
