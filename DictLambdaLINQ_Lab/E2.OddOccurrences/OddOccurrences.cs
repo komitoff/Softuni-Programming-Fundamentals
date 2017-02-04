a0bb9e2363f75e52b58a310f0e83d8148b9ce603
komitoff <tobo_91@abv.bg>
2017-02-04 17:01:20 +0200

    Odd Occurrencies 100/100.

---
+++ DictLambdaLINQ_Lab/E2.OddOccurrences/OddOccurrences.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class OddOccurrences
+{
+    static void Main()
+    {
+        string[] words = Console.ReadLine().Split(' ');
+        Dictionary<string, int> countWords = new Dictionary<string, int>();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            string currentWord = words[i].ToLower();
+            if (!countWords.ContainsKey(currentWord))
+            {
+                countWords.Add(currentWord, 0);
+            }
+            countWords[currentWord]++;
+        }
+        List<string> sortedWords = new List<string>();
+        foreach (var item in countWords)
+        {
+            if (!(item.Value % 2 == 0))
+            {
+                sortedWords.Add(item.Key);
+            }
+        }
+        Console.WriteLine(string.Join(", ", sortedWords));
+    }
+}
