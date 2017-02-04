5ff64f1e910f63239af512f569acd0f96162a0ad
komitoff <tobo_91@abv.bg>
2017-02-04 17:16:08 +0200

    Short word sorted - 100/100.

---
+++ DictLambdaLINQ_Lab/E5.ShortWordSorted/ShortWordSorted.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ShortWordSorted
+{
+    static void Main()
+    {
+        string input = Console.ReadLine().ToLower();
+        List<string> words = input.Split(
+            new char [] { '.', ',', ':', ';', '(', ')', '[', ']', '"', '\'', '\\', '/', '!', '?', ' ' }, 
+            StringSplitOptions.RemoveEmptyEntries)
+            .Where(x => x.Length < 5)
+            .OrderBy(x => x)
+            .Distinct()
+            .ToList();
+
+        Console.WriteLine(string.Join(", ", words));
+    }
+}
