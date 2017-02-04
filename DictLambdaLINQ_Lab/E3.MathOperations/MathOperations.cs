df454e596c1f9d5b4ec5bdd88d8145bf0500c535
komitoff <tobo_91@abv.bg>
2017-02-04 17:41:16 +0200

    Sum Min Max Average - math operations (not checked - judge does not work)

---
+++ DictLambdaLINQ_Lab/E3.MathOperations/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MathOperations
+{
+    static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+        List<int> numbers = new List<int>();
+        for (int i = 0; i < length; i++)
+        {
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+        Console.WriteLine("Sum = {0}", numbers.Sum());
+        Console.WriteLine("Min = {0}", numbers.Min());
+        Console.WriteLine("Max = {0}", numbers.Max());
+        Console.WriteLine("Average = {0}", numbers.Average());
+    }
+}
