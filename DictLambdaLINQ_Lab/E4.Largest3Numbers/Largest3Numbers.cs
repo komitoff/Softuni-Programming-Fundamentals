37181afcc23083a3d0a9fa61b8cb5f5a41c76499
komitoff <tobo_91@abv.bg>
2017-02-04 17:08:05 +0200

    Largest 3 numbers - 100/100.

---
+++ DictLambdaLINQ_Lab/E4.Largest3Numbers/Largest3Numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Largest3Numbers
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        numbers.OrderByDescending(v => v);
+        var sortedList = numbers.OrderByDescending(x => x);
+        var largest3 = sortedList.Take(3);
+        Console.WriteLine(string.Join(" ", largest3));
+    }
+}
