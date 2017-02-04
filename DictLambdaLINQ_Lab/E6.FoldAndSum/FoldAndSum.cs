2b4075fbcf5086129e4e67d012a9d43d55141191
komitoff <tobo_91@abv.bg>
2017-02-04 17:36:58 +0200

    Fold and sum

---
+++ DictLambdaLINQ_Lab/E6.FoldAndSum/FoldAndSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class FoldAndSum
+{
+    static void Main()
+    {
+        List<int> sequnce = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToList();
+        int k = sequnce.Count / 4;
+        int[] leftRow = sequnce.Take(k).Reverse().ToArray();
+        int[] rightRow = sequnce.Skip(3 * k).Take(k).Reverse().ToArray();
+        int[] row1 = leftRow.Concat(rightRow).ToArray();
+        int[] row2 = sequnce.Skip(k).Take(2 * k).ToArray();
+
+        var summ = row1.Select((x, index) => x + row2[index]);
+        Console.WriteLine(string.Join(" ", summ));
+    }
+}
