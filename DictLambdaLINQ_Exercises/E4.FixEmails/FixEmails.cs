376d5863fec61f9f9c8f1a645da34fa43f3f5039
komitoff <tobo_91@abv.bg>
2017-02-05 13:40:01 +0200

    Fix emails

---
+++ DictLambdaLINQ_Exercises/E4.FixEmails/FixEmails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class FixEmails
+{
+    static void Main()
+    {
+        Dictionary<string, string> emailCatalog = new Dictionary<string, string>();
+        string name = Console.ReadLine();
+        while (!name.Equals("stop"))
+        {
+            string email = Console.ReadLine();
+            AddContact(name, email, emailCatalog);
+
+            name = Console.ReadLine();
+        }
+
+        foreach (var contact in emailCatalog)
+        {
+            Console.WriteLine($"{contact.Key} -> {contact.Value}");
+        }
+    }
+
+    private static void AddContact(string name, string email, Dictionary<string, string> emailCatalog)
+    {
+        if (!emailCatalog.ContainsKey(name))
+        {
+            if (!(email[email.Length - 2] == 'u' &&
+            (email[email.Length - 1] == 'k' ||
+             email[email.Length - 1] == 's')))
+            {
+                emailCatalog.Add(name, email);
+            }
+        }
+    }
+}
