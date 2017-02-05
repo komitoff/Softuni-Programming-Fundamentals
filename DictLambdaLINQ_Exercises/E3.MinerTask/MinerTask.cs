899a73337c9080413f5a3df917eacc651bd501af
komitoff <tobo_91@abv.bg>
2017-02-05 13:27:35 +0200

    A miner task

---
+++ DictLambdaLINQ_Exercises/E3.MinerTask/MinerTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MinerTask
+{
+    static void Main()
+    {
+        string material = Console.ReadLine();
+        long quantity = 0;
+        Dictionary<string, long> priceCatalog = new Dictionary<string, long>();
+
+        while (!material.Equals("stop"))
+        {
+            quantity = long.Parse(Console.ReadLine());
+            if (!priceCatalog.ContainsKey(material))
+            {
+                priceCatalog.Add(material, 0);
+            }
+            priceCatalog[material] += quantity;
+
+            material = Console.ReadLine();
+        }
+
+        foreach (var item in priceCatalog)
+        {
+            Console.WriteLine($"{item.Key} -> {item.Value}");
+        }
+    }
+}
