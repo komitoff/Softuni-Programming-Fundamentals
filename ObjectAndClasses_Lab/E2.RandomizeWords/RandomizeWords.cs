cde89e63a03b99d64212e64cb4a62ca77eafe7da
komitoff <tobo_91@abv.bg>
2017-02-06 19:09:45 +0200

    Randomize words - a very simple randomizing algorithm.

---
+++ ObjectAndClasses_Lab/E2.RandomizeWords/RandomizeWords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class RandomizeWords
+{
+    static void Main()
+    {
+        string[] words = Console.ReadLine().Split();
+        Random randomGenerator = new Random();
+        for (int i = 0; i < words.Length; i++)
+        {
+            string currentWord = words[i];
+            int randomIndex = randomGenerator.Next(0, words.Length - 1);
+            string tempWord = words[randomIndex];
+            words[i] = tempWord;
+            words[randomIndex] = currentWord;
+        }
+
+        foreach (var word in words)
+        {
+            Console.WriteLine(word);
+        }
+
+    }
+}
