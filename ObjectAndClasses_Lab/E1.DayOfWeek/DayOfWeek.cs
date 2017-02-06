2b580a4a6689f7da238ccdeebe788d1b4d51e82a
komitoff <tobo_91@abv.bg>
2017-02-06 19:04:47 +0200

    Day of week - using .NET implemented classes.

---
+++ ObjectAndClasses_Lab/E1.DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+class DayOfWeek
+{
+    static void Main()
+    {
+        string inputDate = Console.ReadLine();
+
+        var date = DateTime.ParseExact(inputDate, "d-M-yyyy", CultureInfo.InvariantCulture);
+        Console.WriteLine(date.DayOfWeek);
+    }
+}
