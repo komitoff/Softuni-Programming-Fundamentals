56fef5e6c8d3ec81182b907fd21e444c32bdfd9a
komitoff <tobo_91@abv.bg>
2017-02-07 18:18:05 +0200

    Rectangle Position 100/100

---
+++ ObjectAndClasses_Lab/E6.RectanglePosition/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Rectangle
+{
+    //read two rectangles {left, top, width, height} 
+    public int Left { get; set; }
+    public int Top { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public int Bottom
+    {
+        get
+        {
+            return Top + Height; 
+        }
+    }
+
+    public int Right
+    {
+        get
+        {
+            return Left + Width;
+        }
+    }
+
+    public bool IsInside (Rectangle r)
+    {
+        return (r.Left <= this.Left) &&
+               (r.Right >= this.Right) &&
+               (r.Bottom >= this.Bottom) &&
+               (r.Top <= this.Top);
+    }
+}
---
+++ ObjectAndClasses_Lab/E6.RectanglePosition/RectanglePosition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+class RectanglePosition
+{
+    static void Main()
+    {
+        int[] firstRectangleProps = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] secondRectangleProps = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        //read two rectangles {left, top, width, height} 
+        Rectangle r1 = new Rectangle
+        {
+            Left = firstRectangleProps[0],
+            Top = firstRectangleProps[1],
+            Width = firstRectangleProps[2],
+            Height = firstRectangleProps[3]
+        };
+
+        Rectangle r2 = new Rectangle
+        {
+            Left = secondRectangleProps[0],
+            Top = secondRectangleProps[1],
+            Width = secondRectangleProps[2],
+            Height = secondRectangleProps[3]
+        };
+
+        Console.WriteLine(r1.IsInside(r2) ? "Inside" : "Not inside");
+    }
+}
