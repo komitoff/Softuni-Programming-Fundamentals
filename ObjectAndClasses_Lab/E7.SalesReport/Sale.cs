4fd96e9325d9b588456b5c6834d38aa5621b515c
komitoff <tobo_91@abv.bg>
2017-02-07 18:29:42 +0200

    Sale Reports 100/100.

---
+++ ObjectAndClasses_Lab/E7.SalesReport/Sale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Sale
+{
+    public string Town { get; set; }
+    public string Stock { get; set; }
+    public decimal Price { get; set; }
+    public decimal Quantity { get; set; }
+
+    public Sale ReadSale(string input)
+    {
+        string[] items = input.Split(' ');
+        return new Sale()
+        {
+            Town = items[0],
+            Stock = items[1],
+            Price = decimal.Parse(items[2]),
+            Quantity = decimal.Parse(items[3])
+        };
+    }
+
+}
+
