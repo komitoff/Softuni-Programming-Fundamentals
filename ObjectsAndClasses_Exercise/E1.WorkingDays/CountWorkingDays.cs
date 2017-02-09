56a804cb1b7a65a74a606949c6e1ed642b886097
komitoff <tobo_91@abv.bg>
2017-02-09 19:42:31 +0200

    Count working days  100/100

--- ObjectsAndClasses_Exercise/E1.WorkingDays/CountWorkingDays.cs
+++ ObjectsAndClasses_Exercise/E1.WorkingDays/CountWorkingDays.cs
@@ -7,26 +7,30 @@ public class CountWorkingDays
     {
         string start = Console.ReadLine();
         string end = Console.ReadLine();
-
+        long holydaysCounter = 0;
         DateTime startDate = DateTime.ParseExact(start, "dd-MM-yyyy", CultureInfo.InvariantCulture);
         DateTime endDate = DateTime.ParseExact(end, "dd-MM-yyyy", CultureInfo.InvariantCulture);
         List<DateTime> holydays = new List<DateTime>();
-        holydays.Add(DateTime.ParseExact("01-01-2015", "dd-MM-yyyy", CultureInfo.InvariantCulture));
-        holydays.Add(DateTime.ParseExact("03-03-2015", "dd-MM-yyyy", CultureInfo.InvariantCulture));
-        holydays.Add(DateTime.ParseExact("01-05-2015", "dd-MM-yyyy", CultureInfo.InvariantCulture));
-        holydays.Add(DateTime.ParseExact("06-05-2015", "dd-MM-yyyy", CultureInfo.InvariantCulture));
-        holydays.Add(DateTime.ParseExact("24-05-2015", "dd-MM-yyyy", CultureInfo.InvariantCulture));
-        holydays.Add(DateTime.ParseExact("06-09-2015", "dd-MM-yyyy", CultureInfo.InvariantCulture));
-        holydays.Add(DateTime.ParseExact("22-09-2015", "dd-MM-yyyy", CultureInfo.InvariantCulture));
-        holydays.Add(DateTime.ParseExact("01-11-2015", "dd-MM-yyyy", CultureInfo.InvariantCulture));
-        holydays.Add(DateTime.ParseExact("24-12-2015", "dd-MM-yyyy", CultureInfo.InvariantCulture));
-        holydays.Add(DateTime.ParseExact("25-12-2015", "dd-MM-yyyy", CultureInfo.InvariantCulture));
-        holydays.Add(DateTime.ParseExact("26-12-2015", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+        holydays.Add(DateTime.ParseExact("01-01-2016", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+        holydays.Add(DateTime.ParseExact("03-03-2016", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+        holydays.Add(DateTime.ParseExact("01-05-2016", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+        holydays.Add(DateTime.ParseExact("06-05-2016", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+        holydays.Add(DateTime.ParseExact("24-05-2016", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+        holydays.Add(DateTime.ParseExact("06-09-2016", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+        holydays.Add(DateTime.ParseExact("22-09-2016", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+        holydays.Add(DateTime.ParseExact("01-11-2016", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+        holydays.Add(DateTime.ParseExact("24-12-2016", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+        holydays.Add(DateTime.ParseExact("25-12-2016", "dd-MM-yyyy", CultureInfo.InvariantCulture));
+        holydays.Add(DateTime.ParseExact("26-12-2016", "dd-MM-yyyy", CultureInfo.InvariantCulture));
 
-        for (DateTime i = startDate.d; i <= endDate; i++)
+        for (DateTime currentDate = startDate; currentDate <= endDate; currentDate = currentDate.AddDays(1))
         {
-
+            DateTime checkDate = new DateTime(2016, currentDate.Month, currentDate.Day);
+            if (!holydays.Contains(checkDate) && currentDate.DayOfWeek != DayOfWeek.Sunday && currentDate.DayOfWeek != DayOfWeek.Saturday)
+            {
+                holydaysCounter++;
+            }
         }
-
+        Console.WriteLine(holydaysCounter);
     }
 }
