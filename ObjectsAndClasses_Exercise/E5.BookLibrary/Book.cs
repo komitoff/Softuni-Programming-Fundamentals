87c9db29788be6e6ddb445301c5ff1f4328bd228
komitoff <tobo_91@abv.bg>
2017-02-10 14:04:21 +0200

    Book library

---
+++ ObjectsAndClasses_Exercise/E5.BookLibrary/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public Book(string title, string author)
+    {
+        this.Title = title;
+        this.Author = author;
+    }
+}
---
+++ ObjectsAndClasses_Exercise/E5.BookLibrary/BookLibrary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+public class BookLibrary
+{
+    static void Main()
+    {
+        int count = int.Parse(Console.ReadLine());
+        SortedDictionary<string, decimal> authors = new SortedDictionary<string, decimal>();
+        List<Book> library = new List<Book>();
+        for (int i = 0; i < count; i++)
+        {
+            string[] info = Console.ReadLine().Split();
+            //{title} {author} {publisher} {release date} {ISBN} {price}.
+            string title = info[0];
+            string author = info[1];
+            DateTime releaseDate = DateTime.ParseExact(info[3], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            decimal price = decimal.Parse(info[5]);
+            if (!authors.ContainsKey(author))
+            {
+                authors.Add(author, price);
+            }
+            foreach (var book in library)
+            {
+                if (book.Author.Equals(author))
+                {
+                    authors[author] += price;
+                }
+            }
+            Book currentBook = new Book(title, author);
+            library.Add(currentBook);
+        }
+
+        foreach (var author in authors
+            .OrderByDescending(a => a.Value)
+            .ThenBy(a => a.Key))
+        {
+            Console.WriteLine($"{author.Key} -> {author.Value}");
+        }
+        
+    }
+}
