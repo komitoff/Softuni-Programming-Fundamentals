d7fddaa19b607a22629300801553433ba9778461
komitoff <tobo_91@abv.bg>
2017-02-10 13:18:18 +0200

    Average grades 100/100

---
+++ ObjectsAndClasses_Exercise/E4.AverageGrades/AverageGrades.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class AverageGrades
+{
+    static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+        List<Student> students = new List<Student>();
+
+        for (int i = 0; i < length; i++)
+        {
+            string[] studentsInfo = Console.ReadLine().Split().ToArray();
+            string name = studentsInfo[0];
+            List<double> grades = new List<double>();
+            for (int j = 1; j < studentsInfo.Length; j++)
+            {
+                grades.Add(double.Parse(studentsInfo[j]));
+            }
+            students.Add(new Student(name, grades));
+        }
+
+        foreach (var student in students
+            .Where(s => s.AverageGrade >= 5)
+            .OrderBy(s => s.Name)
+            .ThenByDescending(s => s.AverageGrade))
+        {
+            Console.WriteLine($"{student.Name} -> {student.AverageGrade:f2}");
+        }
+    }
+}
---
+++ ObjectsAndClasses_Exercise/E4.AverageGrades/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Student
+{
+    public string Name { get; set; }
+    public List<double> Grades { get; set; }
+    public double AverageGrade { get; set; }
+
+    public Student(string name, List<double> grades)
+    {
+        this.Name = name;
+        this.Grades = grades;
+        this.AverageGrade = this.Grades.Average(x => x);
+    }
+
+
+}
+
