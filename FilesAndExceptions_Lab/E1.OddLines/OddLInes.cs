0848f52036919f0529f76baefface40505d9bf90
komitoff <tobo_91@abv.bg>
2017-02-11 20:14:06 +0200

    Read text from file and write the odd lines to another file.

---
+++ FilesAndExceptions_Lab/E1.OddLines/OddLInes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+public class OddLInes
+{
+    static void Main()
+    {
+        string[] textLines = File.ReadAllLines("input.txt");
+        List<string> oddLines = new List<string>();
+
+        for (int i = 0; i < textLines.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                oddLines.Add(textLines[i]);
+            }
+        }
+
+        File.WriteAllLines("output.txt", oddLines);
+
+    }
+}
