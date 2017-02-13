f0ddedfd47f3a871888044672a52f58c5965acfc
komitoff <tobo_91@abv.bg>
2017-02-13 18:41:53 +0200

    String and text homework. First task is done - to reverse a string.

---
+++ StringAndText_Lab/E1.ReverseString/ReverseString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class ReverseString
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        char[] letters = input.ToCharArray();
+        Array.Reverse(letters);
+        string reversed = new string(letters);
+        Console.WriteLine(reversed);
+
+    }
+}
