0400965df8ffe14dc1af6847715cec89e3e2ecb7
komitoff <tobo_91@abv.bg>
2017-02-13 18:58:10 +0200

    Count substring occurrences in text

---
+++ StringAndText_Lab/E2.CountSubstringOccurrences/CountSubstrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class CountSubstrings
+{
+    static void Main()
+    {
+        string input = Console.ReadLine().ToLower();
+        string substring = Console.ReadLine().ToLower();
+        int index = input.IndexOf(substring);
+        int cnt = 0;
+        while (index != -1)
+        {
+            cnt++;
+            index = input.IndexOf(substring, index + 1);
+        }
+        Console.WriteLine(cnt);
+    }
+}
