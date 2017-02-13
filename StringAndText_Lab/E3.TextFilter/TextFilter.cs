c886371bbabce16f0f61a36576891c6ef83f49e9
komitoff <tobo_91@abv.bg>
2017-02-13 19:07:18 +0200

    Replacing words in text with *

---
+++ StringAndText_Lab/E3.TextFilter/TextFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class TextFilter
+{
+    static void Main()
+    {
+        string[] bannedWords = Console.ReadLine().Split(new char[] { ',', ' '}, StringSplitOptions.RemoveEmptyEntries);
+        string text = Console.ReadLine();
+        int index = 0;
+        for (int i = 0; i < bannedWords.Length; i++)
+        {
+            if (text.Contains(bannedWords[i]))
+            {
+                text = text.Replace(bannedWords[i], new string('*', bannedWords[i].Length));
+            }
+        }
+
+        Console.WriteLine(text);
+    }
+}
