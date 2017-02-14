dd03a6a9bd1f95740946b9e5df8bcbdf39bef0a1
komitoff <tobo_91@abv.bg>
2017-02-14 20:53:03 +0200

    Magic exchangeable words - not finished yet.

---
+++ Strings_Exercises/MagicExchangeableWords/MagicExchangeableWords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class MagicExchangeableWords
+{
+    static void Main()
+    {
+        string[] words = Console.ReadLine().Split();
+        string firstWord = words[0];
+        string secondWord = words[1];
+        Dictionary<char, char> mappedChars = new Dictionary<char, char>();
+
+    }
+}
