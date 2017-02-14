0c57c9702bf01f64531fbd7b707fbb67e80c044b
komitoff <tobo_91@abv.bg>
2017-02-14 20:24:48 +0200

    Unicode characters from give string.

---
+++ Strings_Exercises/UnicodeCharacters/UnicodeCharacters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class UnicodeCharacters
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        StringBuilder result = new StringBuilder();
+        foreach (char letter in input)
+            result.AppendFormat("\\u{0:X4}".ToLower(), (int)letter);
+        Console.WriteLine(result);
+    }
+}
