9631c385fe734ee220d6c68a0b3b4fad9ba0e756
komitoff <tobo_91@abv.bg>
2017-02-14 19:53:06 +0200

    10 base to N base (without with simple algorithm)

---
+++ Strings_Exercises/ConvertBase10ToBaseN/BaseConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+public class BaseConverter
+{
+    static void Main()
+    {
+        string[] numberAndBase = Console.ReadLine().Split();
+        BigInteger number = BigInteger.Parse(numberAndBase[1]);
+        BigInteger nBase = BigInteger.Parse(numberAndBase[0]);
+        StringBuilder output = ConvertToNBase(number, nBase);
+        Console.WriteLine(output);
+    }
+
+    private static StringBuilder ConvertToNBase(BigInteger number, BigInteger nBase)
+    {
+        StringBuilder result = new StringBuilder();
+        List<BigInteger> rem = new List<BigInteger>();
+        if (nBase > 0)
+        {
+            while (number > 0)
+            {
+                rem.Add(number % nBase);
+                number /= nBase;
+            }
+            for (int i = rem.Count - 1; i >= 0; i--)
+            {
+                result.Append(rem[i]);
+            }
+        }
+        return result;
+    }
+}
