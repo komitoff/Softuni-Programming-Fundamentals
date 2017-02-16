12ce93778ba1e48ee451ce9cc689d19ec50e589d
komitoff <tobo_91@abv.bg>
2017-02-16 18:05:43 +0200

    Playing with simple regex examples.

---
+++ Regex_Lab_Exercises/MatchFullName/MatchFullName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class MatchFullName
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+
+        string pattern = @"[A-Z][a-z]+\s+[A-Z][a-z]+";
+        Regex rgx = new Regex(pattern);
+        MatchCollection matches = rgx.Matches(text);
+        Console.WriteLine($"found {matches.Count} matches");
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match);
+        }
+    }
+}
---
+++ Regex_Lab_Exercises/MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class MatchPhoneNumber
+{
+    static void Main()
+    {
+        string number = Console.ReadLine();
+        string pattern = @"\+[0-9]{3}\s+[0-9]{1}\s+[0-9]{3}\s+[0-9]{4}";
+        Regex rgx = new Regex(pattern);
+        MatchCollection matches = rgx.Matches(number);
+        Console.WriteLine("Found {0} results ", matches.Count);
+
+        foreach (Match match in matches)
+        {
+            Console.WriteLine("Found : {0}", match);
+        }
+
+    }
+}
