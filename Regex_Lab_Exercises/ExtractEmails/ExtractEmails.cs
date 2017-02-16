8881c56365e3511cc99c4e69ba12f7238436c6e9
komitoff <tobo_91@abv.bg>
2017-02-16 19:34:55 +0200

    Extract emails - done (not checked in judge)

---
+++ Regex_Lab_Exercises/ExtractEmails/ExtractEmails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class ExtractEmails
+{
+    static void Main()
+    {
+        string inputText = Console.ReadLine();
+        string pattern = @"[A-Za-z0-9.%-_+-]+@[A-Za-z0-9%-_+-]+\.[a-z]+.*?";
+        Regex rgx = new Regex(pattern);
+        MatchCollection matches = rgx.Matches(inputText);
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match);
+        }
+    }
+}
