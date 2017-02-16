f0d6b222ff3bfc939a9b6f8ad4eeec56f3b95072
komitoff <tobo_91@abv.bg>
2017-02-16 19:44:26 +0200

    Find a valid sentence in text (solved without regular expressions atm).

---
+++ Regex_Lab_Exercises/ExtractSentenceByKeyword/ExtractSentenceByKeyword.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class ExtractSentenceByKeyword
+{
+    static void Main()
+    {
+        string keyword = Console.ReadLine();
+        string[] text = Console.ReadLine()
+            .Split(new char[] { '.', '!', '?' },
+            StringSplitOptions.RemoveEmptyEntries);
+        foreach (var sentence in text)
+        {
+            string[] wordsInSentence = sentence
+                .Split(new char[] { ' ', ',', '-', ':', ';' },
+                StringSplitOptions.RemoveEmptyEntries);
+            foreach (var word in wordsInSentence)
+            {
+                if (keyword.Equals(word))
+                {
+                    Console.WriteLine(sentence.TrimStart());
+                }
+            }
+        }
+    }
+}
