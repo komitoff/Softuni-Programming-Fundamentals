83e71cf303acde8240b0eb6f8ec6bd7fdfca23ca
komitoff <tobo_91@abv.bg>
2017-02-16 17:47:29 +0200

    Replace a tag 100/100 - from the lab

---
+++ Regex_Lab_Exercises/ReplaceTag/ReplaceTag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class ReplaceTag
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        while(!text.Equals("end"))
+        {
+            //               @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+            string pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+            string replace = @"[URL href=$1]$2[/URL]";
+            string replaced = Regex.Replace(text, pattern, replace);
+            Console.WriteLine(replaced);
+            text = Console.ReadLine();
+        }
+    }
+}
