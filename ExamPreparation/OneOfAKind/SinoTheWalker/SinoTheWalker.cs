a3ab3afd48de985333960de6bc8cdd1904c2e14a
komitoff <tobo_91@abv.bg>
2017-02-18 13:57:17 +0200

    Sino the walker 100/100 using TimeSpan

---
+++ ExamPreparation/OneOfAKind/SinoTheWalker/SinoTheWalker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+public class SinoTheWalker
+{
+    static void Main()
+    {
+        TimeSpan time = TimeSpan.Parse(Console.ReadLine());
+        int steps = int.Parse(Console.ReadLine()) % 86400;
+        int secondsPerStep = int.Parse(Console.ReadLine()) % 86400;
+
+        long sum = steps * secondsPerStep;
+        TimeSpan timeInSeconds = TimeSpan.FromSeconds(sum);
+
+        TimeSpan totalTime = timeInSeconds + time;
+
+        Console.WriteLine("Time Arrival: {0:hh\\:mm\\:ss}", totalTime);
+
+
+
+    }
+}
