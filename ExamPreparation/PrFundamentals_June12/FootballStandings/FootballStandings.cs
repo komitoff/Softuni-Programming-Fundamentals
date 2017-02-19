9eed8da8cc6e8aff677b0fe87c16c5bed9dcf54c
komitoff <tobo_91@abv.bg>
2017-02-19 13:36:42 +0200

    Football standings - must be finished.

---
+++ ExamPreparation/PrFundamentals_June12/FootballStandings/FootballStandings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+public class FootballStandings
+{
+    static void Main()
+    {
+        string decryptCode = Console.ReadLine().ToUpper();
+        //team / score
+        Dictionary<string, int> teamScores = new Dictionary<string, int>();
+        //team / total goals
+        Dictionary<string, long> teamGoals = new Dictionary<string, long>();
+
+        string match = Console.ReadLine().ToUpper();
+
+        while (!match.Equals("FINAL"))
+        {
+            string[] tokens = match.Split(new string[] { decryptCode }, StringSplitOptions.RemoveEmptyEntries);
+            string matchResult = tokens[tokens.Length - 1];
+            Console.WriteLine(matchResult);
+            string[] teams = new string[2];
+            for (int i = 0, j = 0; i < tokens.Length; i++)
+            {
+                if (tokens[i].IndexOf(decryptCode) != -1 && tokens[i].LastIndexOf(decryptCode) != -1)
+                {
+                    teams[j] = tokens[i];
+                    j++;
+                }
+            }
+            foreach (var tea in teams)
+            {
+                Console.WriteLine(tea);
+            }
+            match = Console.ReadLine().ToUpper();
+        }
+    }
+}
